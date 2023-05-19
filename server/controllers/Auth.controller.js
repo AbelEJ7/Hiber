970d8170c079aaeafc32bfadee493db1c1138abc
leula7 <leulkahssaye1000@gmail.com>
2023-05-19 12:30:53 +0300

    make readable by changing to async task

--- server/controllers/Auth.controller.js
+++ server/controllers/Auth.controller.js
@@ -1,27 +1,33 @@
 import Login from "../models/login.models.js";
+import express from 'express';
+import bodyParser from 'body-parser';
 
-export const authenticateUser = (req, res) => {
-  const { username, password } = req.body;
+const app = express();
 
-  Login.findOne({ username: { $regex: new RegExp(`^${username}$`, "i") } }, (err, user) => {
-    if (err) {
-      console.log('Error finding user:', err);
-      res.status(500).json({ message: 'Internal server error' });
-      return;
-    }
+// Parse JSON bodies
+app.use(bodyParser.json());
 
-    if (!user) {
-      console.log('User not found');
-      res.status(401).json({ message: 'Invalid credentials' });
-      return;
-    }
-
-    if (user.password === password) {
-      console.log('Authentication successful');
-      res.status(200).json({ message: 'Authentication successful' });
-    } else {
-      console.log('Invalid password');
-      res.status(401).json({ message: 'Invalid credentials' });
-    }
-  });
+export const authenticateUser = async (req, res) => {
+    const { username, password } = req.body;
+    console.log(username," ",password," body")
+    // Find the user by username
+    try {
+        const user = await Login.findOne({ username });
+        if (!user) {
+          console.log('User not found');
+          return res.status(401).json({ message: 'Invalid username' });
+        }
+      
+        if (user.password === password) {
+          console.log('Authentication successful');
+          return res.status(200).json({ message: 'Authentication successful' });
+          // Perform further actions for an authenticated user
+        } else {
+          console.log('Invalid password');
+          return res.status(401).json({ message: 'Invalid password' });
+        }
+      } catch (error) {
+        console.log('Error finding user:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
 };
